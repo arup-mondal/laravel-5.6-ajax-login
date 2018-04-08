7797ee72fd4bdcd6bb1acbbabd6577b5e113972d
unknown <arup2109@gmail.com>
2018-04-08 10:42:43 +0530

    AJAX authentication

---
+++ resources/assets/js/login.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	//Login form validation
+		$('#loginForm').validate({
+			rules:{
+				email:{
+					required:true,
+					email:true
+				},
+				password:{
+					required:true
+				}
+			},
+			messages:{
+				email:{
+					required:"Please enter an email address.",
+					email:"Please enter a valid email address."
+				},
+				password:{
+					required:"Please enter a password."
+				}
+			},
+			submitHandler:function(form){
+				var formurl = $(form).attr('action');
+				$.ajax({
+		            url: formurl , 
+		            type: "POST",             
+		            data: new FormData(form),
+		            cache: false,             
+		            processData: false,
+	  				contentType: false,      
+		            success: function(data) {
+		            	if(data.auth)
+		            		window.location.href = SITE_URL+data.intended;
+		            	else
+		            		$('#loginForm #errorMsg').text('Some error occured, please try again!');
+		            },
+		            error:function(xhr){
+		            	console.log(xhr.responseJSON.message);
+		            	$('#loginForm #errorMsg').text(xhr.responseJSON.message);
+		            }
+		        });
+			}
+		})
+	//Login form validation
+});	
